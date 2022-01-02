bfd27e83d03ba47b0c800a136b971d848c95313a
himitery <himit0131@gmail.com>
2022-01-02 22:23:54 +0900

    프로젝트 환경설정: 프로젝트 생성

---
+++ src/main/java/study/springboot/webapplicationdevelopment/WebApplicationDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package study.springboot.webapplicationdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplicationDevelopmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/springboot/webapplicationdevelopment/WebApplicationDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springboot.webapplicationdevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
