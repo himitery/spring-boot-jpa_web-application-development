f6d0e489adf1425701d6b17aec55de91107d355e
himitery <himit0131@gmail.com>
2022-01-29 02:43:01 +0900

    상품 도메인 개발: 상품 리포지토리 개발

---
+++ src/main/java/study/springboot/webapplicationdevelopment/repository/ItemRepository.java
@@ -0,0 +1,31 @@
+package study.springboot.webapplicationdevelopment.repository;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import study.springboot.webapplicationdevelopment.domain.item.Item;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+	private final EntityManager em;
+
+	public void save(Item item) {
+		if (item.getId() == null) {
+			em.persist(item);
+		} else {
+			em.merge(item);
+		}
+	}
+
+	public Item findOne(Long id) {
+		return em.find(Item.class, id);
+	}
+
+	public List<Item> findAll() {
+		return em.createQuery("select i from Item i", Item.class)
+			.getResultList();
+	}
+}
