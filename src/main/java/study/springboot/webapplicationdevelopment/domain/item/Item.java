f51ff897a865dc53fc0490d0c6131e28e2e6f2cf
himitery <himit0131@gmail.com>
2022-01-29 02:39:41 +0900

    상품 도메인 개발: 상품 엔티티 개발(비즈니스 로직 추가)

--- src/main/java/study/springboot/webapplicationdevelopment/domain/item/Item.java
+++ src/main/java/study/springboot/webapplicationdevelopment/domain/item/Item.java
@@ -13,6 +13,7 @@ import javax.persistence.ManyToMany;
 import lombok.Getter;
 import lombok.Setter;
 import study.springboot.webapplicationdevelopment.domain.Category;
+import study.springboot.webapplicationdevelopment.exception.NotEnoughStockException;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -34,4 +35,23 @@ public abstract class Item {
 
 	@ManyToMany(mappedBy = "items")
 	private List<Category> categories = new ArrayList<>();
+
+	//==비즈니스 로직==//
+	/*
+	 * stock 증가
+	 * */
+	public void addStock(int quantity) {
+		this.stockQuantity += quantity;
+	}
+
+	/*
+	 * stock 감소
+	 * */
+	public void removeStock(int quantity) {
+		int restStock = this.stockQuantity - quantity;
+		if (restStock < 0) {
+			throw new NotEnoughStockException("need more stock");
+		}
+		this.stockQuantity = restStock;
+	}
 }
